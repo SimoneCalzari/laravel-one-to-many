9aa12d0bf55d3305f04e86dac612fb0b66a73127
SimoneCalzari <simone.calzari@outlook.it>
2024-02-09 15:07:01 +0100

    feat: add random FK to Projects Table

--- database/seeders/ProjectsTableSeeder.php
+++ database/seeders/ProjectsTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -15,10 +16,14 @@ class ProjectsTableSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
-
+        Project::truncate();
         for ($i = 0; $i < 20; $i++) {
+
+            $type = Type::inRandomOrder()->first();
+
             $project = new Project();
             $project->title = $faker->sentence(3);
+            $project->type_id = $type->id;
             $project->description = $faker->paragraph();
             $project->slug = Str::of($project->title)->slug('-');
             $project->technologies_stack = $faker->words(3, true);
